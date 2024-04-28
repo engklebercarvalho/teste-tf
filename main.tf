bd6d9cb5ced0a679dcbf82563996a8854f7c30f5
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-28 16:47:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ resource "azurerm_linux_virtual_machine" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_DS1_v2"
-  disable_password_authentication = true
   admin_username      = var.vm_username
   admin_password      = var.vm_password
   network_interface_ids = [
