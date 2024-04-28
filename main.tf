d1d4affaeb41c524bca16ff85072504b25cc476b
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-28 16:44:10 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   admin_username      = var.vm_username
   admin_password      = var.vm_password
   disable_password_authentication = false
+#
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
