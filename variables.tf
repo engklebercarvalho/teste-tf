9725f5f6a0e7c7b53b15a6eadc3b2a9bb207a3cf
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-28 16:14:14 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "azure_client_id" {
+  description = "Client ID for Azure authentication"
+}
+
+variable "azure_client_secret" {
+  description = "Client Secret for Azure authentication"
+}
+
+variable "azure_subscription_id" {
+  description = "Subscription ID for Azure"
+}
+
+variable "azure_tenant_id" {
+  description = "Tenant ID for Azure"
+}
