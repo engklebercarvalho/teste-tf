b69b9e8f8ae754bd6879bd2f1b0c0d4edb53369d
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-28 21:38:18 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "<azure_subscription_id>"
+  tenant_id         = "<azure_subscription_tenant_id>"
+  client_id         = "<service_principal_appid>"
+  client_secret     = "<service_principal_password>"
+}
+
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "Brazil South"
