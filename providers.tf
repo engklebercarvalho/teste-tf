735ea1b7e28147a44769cc7b484c646b49a3e5a5
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-29 09:44:38 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,3 @@
-variable "resource_group_name" { type = string }
-variable "storage_account_name" { type = string }
-variable "container_name" { type = string }
-variable "key" { type = string }
-
 terraform {
   required_providers {
     azurerm = {
@@ -11,15 +6,7 @@ terraform {
     }
   }
 
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = var.key
-   
-  }
-}
+
 
 provider "azurerm" {
   features {}
