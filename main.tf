efc1b5e9c9a2f4cf81e66dcaedc32712258c498d
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-29 09:28:58 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-    }
-  }
 
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "RG-MC"
-    storage_account_name = "sakleberstatefile"
-    container_name       = "tfstate"
-    key                  = "1yB88JRCj+jeRKGFgtB5EJBuR3j7G+vCWR4sAIZJBtVVEqK8dHlV64dt6atyyM6MSsvbf3rIpBGh+AStLY40pA=="
-   
-  }
-}
-
-provider "azurerm" {
-  features {}
-
-}
 
 
 resource "azurerm_resource_group" "example" {
