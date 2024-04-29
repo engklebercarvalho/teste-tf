7b023a73981f4e5248300c8e3722f2d491f1d2b0
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-28 22:35:10 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "RG-MC"
-    storage_account_name = "sakleberstatefile "
+    storage_account_name = "sakleberstatefile"
     container_name       = "tfstate"
     key                  = "1yB88JRCj+jeRKGFgtB5EJBuR3j7G+vCWR4sAIZJBtVVEqK8dHlV64dt6atyyM6MSsvbf3rIpBGh+AStLY40pA=="
    
