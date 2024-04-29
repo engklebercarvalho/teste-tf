5120015972cd3d7b6bb450ba2d07695de36b52ba
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-29 09:48:07 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,26 @@ variable "storage_account_name" { type = string }
 variable "container_name" { type = string }
 variable "key" { type = string }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
+   
+  }
+
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
