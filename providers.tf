d7abeb8aed96227ed89e030bed44ae7d2820ec1d
Kleber Santos de Carvalho <75149053+engklebercarvalho@users.noreply.github.com>
2024-04-28 21:36:59 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
 
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
+  subscription_id   = "<azure_subscription_id>"
+  tenant_id         = "<azure_subscription_tenant_id>"
+  client_id         = "<service_principal_appid>"
+  client_secret     = "<service_principal_password>"
 }
